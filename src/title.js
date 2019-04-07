a2b32f351d618294e9cbb233fe3ea0cf43b25011
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2019-04-07 12:31:08 -0400

    TitleComponent
    Finished up the titlebox, added a hacker news icon

--- src/title.js
+++ src/title.js
@@ -2,7 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 var Title = () => {
-  return <div className="titleBox">HackerNews</div>;
+  return (
+    <ul className="titleBox">
+      <li className="fab fa-hacker-news" />
+      <li className="listInfo">
+        <b>HackerNews</b>
+      </li>
+      <li className="listInfo">new|</li>
+      <li className="listInfo">threads|</li>
+      <li className="listInfo">comments|</li>
+      <li className="listInfo">show|</li>
+      <li className="listInfo">ask|</li>
+      <li className="listInfo">jobs|</li>
+      <li className="listInfo">submit</li>
+    </ul>
+  );
 };
 
 export default Title;
